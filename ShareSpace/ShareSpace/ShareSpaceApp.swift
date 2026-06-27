4539d9f1c20c8d443a4e56d5c6c871c3318a866c
smriti <smriti@singsys.com>
2026-06-27 13:39:08 +0530

    initial commit

---
+++ ShareSpace/ShareSpace/ShareSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSpaceApp.swift
+//  ShareSpace
+//
+//  Created by singsys on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ShareSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShareSpace/ShareSpaceTests/ShareSpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSpaceTests.swift
+//  ShareSpaceTests
+//
+//  Created by singsys on 27/06/26.
+//
+
+import Testing
+@testable import ShareSpace
+
+struct ShareSpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
